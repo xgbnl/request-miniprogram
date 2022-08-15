b4c7d29b5583abe1c2516192488fab4958d79bd5
xgbnl <745502274@qq.com>
2022-08-15 22:12:07 +0800

    feat: 新增token检查方法

--- src/Config/AppConfig.js
+++ src/Config/AppConfig.js
@@ -7,6 +7,14 @@ export class AppConfig {
     constructor() {
     }
 
+    /**
+     * Init singleton setting.
+     * @param api request api
+     * @param loginPage user login page
+     * @param homePage miniProgram home page
+     * @param tokenType token type
+     * @param storageKey token storage key
+     */
     configure({api, loginPage, homePage,tokenType,storageKey}) {
         this.#globalData = {
             api ,
@@ -17,26 +25,58 @@ export class AppConfig {
         };
     }
 
+    /**
+     * Get request api.
+     * @returns {string}
+     */
     getApi() {
         return this.#globalData.api;
     }
 
+    /**
+     * Get miniProgram login pages.
+     * @returns {string}
+     */
     getLoginPage() {
         return this.#globalData.loginPage;
     }
 
+    /**
+     * Get miniProgram home pages.
+     * @returns {*}
+     */
     getHomePage() {
         return this.#globalData.homePage;
     }
 
+    /**
+     * Get token type.{bearer}
+     * @returns {string}
+     */
     getTokenType() {
         return this.#globalData.tokenType;
     }
 
+    /**
+     * Get token storage key.
+     * @returns {string}
+     */
     getStorageKey() {
         return this.#globalData.storageKey;
     }
 
+    /**
+     * Check access token is empty.
+     * @returns {boolean}
+     */
+    tokenIsEmpty(){
+        return !wx.getStorageSync(this.getStorageKey());
+    }
+
+    /**
+     * Get AppConfig singleton instance.
+     * @returns {AppConfig}
+     */
     static getInstance() {
         if (!this.#instance) {
             this.#instance = new AppConfig();
