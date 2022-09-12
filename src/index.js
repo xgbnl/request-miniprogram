c0cd0067d77a17ba24dd893d87fd240f0bbc2cf4
xgbnl <745502274@qq.com>
2022-09-12 14:26:02 +0800

    chore: 优化入口文件

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import {HttpFactory} from "./Factories/HttpFactory";
-import {AppConfig} from "./Config/AppConfig";
-import {Helper} from "./Helper/Helper";
-
-const request = HttpFactory.getRequest();
+import { HttpFactory } from "./Factories/HttpFactory";
+import { AppConfig } from "./Services/AppConfig";
+import { Helper } from "./Helper/Helper";
+import { Token } from './Services/Token';
+import { InterceptorFactory } from './Factories';
 
 const appConfig = AppConfig.getInstance();
 
-export {
-    request,
+const token = new Token(AppConfig);
+
+const restful = HttpFactory.getRestful(
+    InterceptorFactory.getRequestInterceptor(),
+    InterceptorFactory.getResponseInterceptor(appConfig),
     appConfig,
+    token,
+);
+
+export {
+    restful as RESTFul,
+    appConfig as AppConfig,
     Helper,
+    token as Token,
 };
